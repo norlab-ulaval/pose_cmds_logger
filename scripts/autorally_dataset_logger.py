e8ba50a83a9d49244108a548da1debbc608a674e
DomBaril <dominic.baril@usherbrooke.ca>
2022-04-21 14:16:59 -0400

    now working with a service to save all of the data

--- scripts/autorally_dataset_logger.py
+++ scripts/autorally_dataset_logger.py
@@ -3,7 +3,7 @@ import rospy
 from std_msgs.msg import Float64, Bool
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
-
+from sensor_msgs.msg import Imu
 import numpy as np
 import pandas as pd
 
@@ -12,10 +12,11 @@ class Server:
         self.calib_switch = Bool()
         self.joy_switch = Bool()
         self.pose = Odometry()
-        self.velocity_left_cmd = Float64()
-        self.velocity_left_meas = Float64()
-        self.velocity_right_cmd = Float64()
-        self.velocity_right_meas = Float64()
+        self.imu = Imu()
+        # self.velocity_left_cmd = Float64()
+        # self.velocity_left_meas = Float64()
+        # self.velocity_right_cmd = Float64()
+        # self.velocity_right_meas = Float64()
 
     def switch_callback(self, msg):
         # "Store" message received.
@@ -29,28 +30,20 @@ class Server:
         # "Store" message received.
         self.pose = msg
 
-    def velocity_left_cmd_callback(self, msg):
-        # "Store" the message received.
-        self.velocity_left_cmd = msg
+    def imu_callback(self, msg):
+        # "Store" message received.
+        self.imu = msg
 
-    def velocity_left_meas_callback(self, msg):
-        # "Store" the message received.
-        self.velocity_left_meas = msg
-
-    def velocity_right_cmd_callback(self, msg):
-        # "Store" the message received.
-        self.velocity_right_cmd = msg
-
-    def velocity_right_meas_callback(self, msg):
-        # "Store" the message received.
-        self.velocity_right_meas = msg
+    def twist_callback(self, msg):
+        # "Store" message received.
+        self.twist = msg
 
     def log_msgs(self, array):
         # Create numpy array with adequate poses
         global prev_icp_x
         global prev_icp_y
         global icp_index
-        new_row = np.zeros((1, 13))
+        new_row = np.zeros((1, 16))
 
         if (self.pose.pose.pose.position.x != prev_icp_x
                 and self.pose.pose.pose.position.y != prev_icp_y):
@@ -58,20 +51,24 @@ class Server:
             prev_icp_y = self.pose.pose.pose.position.y
             icp_index += 1
 
-        new_row = np.array(([rospy.get_rostime(), self.joy_switch.data, icp_index,
-                             self.velocity_left_cmd.data, self.velocity_left_meas.data,
-                             self.velocity_right_cmd.data, self.velocity_right_meas.data,
+        new_row = np.array(([rospy.get_rostime(), self.joy_switch.data, icp_index, # util data
+                             self.twist.linear.x, self.twist.angular.z, # cmd data
+                             self.imu.orientation.x, self.imu.orientation.y,
+                             self.imu.orientation.z, self.imu.orientation.z,
+                             self.imu.angular_velocity.z, # IMU data
                              self.pose.pose.pose.position.x, self.pose.pose.pose.position.y,
                              self.pose.pose.pose.orientation.x, self.pose.pose.pose.orientation.y,
-                             self.pose.pose.pose.orientation.z, self.pose.pose.pose.orientation.w]))
+                             self.pose.pose.pose.orientation.z, self.pose.pose.pose.orientation.w])) # icp pose data
 
         return np.vstack((array, new_row))
 
     def export_array(self, array):
         rospy.loginfo('Converting Array to DataFrame')
         df = pd.DataFrame(data=array, columns=['ros_time', 'joy_switch', 'icp_index',
-                                               'cmd_left_vel', 'meas_left_vel',
-                                               'cmd_right_vel', 'meas_right_vel',
+                                               'cmd_lin', 'cmd_ang',
+                                               'imu_quat_x', 'imu_quat_y',
+                                               'imu_quat_z', 'imu_quat_w',
+                                               'imu_yaw_rate',
                                                'icp_pos_x', 'icp_pos_y',
                                                'icp_quat_x', 'icp_quat_y',
                                                'icp_quat_z', 'icp_quat_w'])
@@ -88,12 +85,10 @@ if __name__ == '__main__':
     rospy.Subscriber('calib_switch', Bool, server.switch_callback)
     rospy.Subscriber('joy_switch', Bool, server.joy_callback)
     rospy.Subscriber('/icp_odom', Odometry , server.pose_callback)
-    rospy.Subscriber('/left_drive/velocity', Float64, server.velocity_left_cmd_callback)
-    rospy.Subscriber('/left_drive/status/speed', Float64, server.velocity_left_meas_callback)
-    rospy.Subscriber('/right_drive/velocity', Float64, server.velocity_right_cmd_callback)
-    rospy.Subscriber('/right_drive/status/speed', Float64, server.velocity_right_meas_callback)
+    rospy.Subscriber('/imu/data', Imu , server.imu_callback)
+    rospy.Subscriber('cmd_vel', Twist, server.twist_callback)
 
-    array = np.zeros((1, 13))
+    array = np.zeros((1, 16))
     odom_index = 0
     global prev_icp_x
     prev_icp_x = 0
