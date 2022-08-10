9e686a9906661906ff72c608465b44e78797a75f
DomBaril <dominic.baril@usherbrooke.ca>
2022-08-10 11:23:46 -0400

    added commanded body vels

--- scripts/jointstates_cmds_logger_node.py
+++ scripts/jointstates_cmds_logger_node.py
@@ -18,6 +18,7 @@ pose = Odometry()
 joint_state = JointState()
 joint_state.position = [0, 0, 0, 0]
 imu_vel = Imu()
+cmd_vel = Twist()
 
 def switch_callback(msg):
     # "Store" message received.
@@ -48,12 +49,16 @@ def imu_callback(msg):
     global imu_vel
     imu_vel = msg
 
+def cmd_vel_callback(msg):
+    global cmd_vel
+    cmd_vel = msg
+
 def log_msgs(array):
     # Create numpy array with adequate poses
     global prev_icp_x
     global prev_icp_y
     global icp_index
-    new_row = np.zeros((1, 16))
+    new_row = np.zeros((1, 18))
 
     if (pose.pose.pose.position.x != prev_icp_x
             and pose.pose.pose.position.y != prev_icp_y):
@@ -63,6 +68,7 @@ def log_msgs(array):
 
     new_row = np.array(([rospy.get_rostime(), joy_switch.data, icp_index, good_calib_step.data,
                          joint_state.position[0], joint_state.position[1],
+                         cmd_vel.linear.x, cmd_vel.angular.z,
                          pose.pose.pose.position.x, pose.pose.pose.position.y, pose.pose.pose.position.z,
                          pose.pose.pose.orientation.x, pose.pose.pose.orientation.y,
                          pose.pose.pose.orientation.z, pose.pose.pose.orientation.w,
@@ -75,6 +81,7 @@ def save_data_handle(req, array):
         rospy.loginfo('Converting Array to DataFrame')
         df = pd.DataFrame(data=array, columns=['ros_time', 'joy_switch', 'icp_index', 'good_calib_step',
                                                'wheel_pos_left', 'wheel_pos_right',
+                                               'cmd_vel_x', 'cmd_vel_y',
                                                'icp_pos_x', 'icp_pos_y', 'icp_pos_z',
                                                'icp_quat_x', 'icp_quat_y',
                                                'icp_quat_z', 'icp_quat_w',
@@ -95,10 +102,11 @@ if __name__ == '__main__':
     icp_sub = rospy.Subscriber('/icp_odom', Odometry , pose_callback)
     cmd_left_sub = rospy.Subscriber('/joint_states', JointState, jointstate_callback)
     imu_sub = rospy.Subscriber('/MTI_imu/data_raw', Imu, imu_callback)
+    cmd_vel_sub = rospy.Subscriber('/doughnut_cmd_vel', Twist, cmd_vel_callback)
 
     save_service = rospy.Service('save_data', SaveData, lambda msg: save_data_handle(msg, array))
 
-    array = np.zeros((1, 16))
+    array = np.zeros((1, 18))
     odom_index = 0
     global prev_icp_x
     prev_icp_x = 0
